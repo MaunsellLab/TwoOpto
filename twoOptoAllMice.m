02af8559a2fcd928db332f3d9d499d7a93bf3af4
JJCone <114761551+Corticomics@users.noreply.github.com>
2023-07-04 15:29:39 -0600

    updated mice

--- twoOptoAllMice.m
+++ twoOptoAllMice.m
@@ -3,7 +3,7 @@ addpath '/Users/jacksoncone/Documents/GitHub/TwoOpto';
 % Grab twoOpto Data and plot a heatmap of impairment by condition
 
 % List of Animals in the twoOpto GitHub Folder
-animals = {'2339','2365','2394','2396','2401','2454','2456','2475','2488'};
+%animals = {'2339','2365','2394','2396','2401','2454','2456','2475','2488'};
 
 % Set this to the location of the data files on your machine
 [~, name] = system('hostname');
