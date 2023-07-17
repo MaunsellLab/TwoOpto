c5e89b96ee33b6fadfd231fca14fb26bbfb9846f
JJCone <114761551+Corticomics@users.noreply.github.com>
2023-07-17 13:37:03 -0600

    Update probSim.m

--- probSim.m
+++ probSim.m
@@ -39,6 +39,67 @@
 
 % We observe delta hit rate/delta d' for s0, s1, and s0+s1
 
+%% Get Actual Performance Values
+[masterStruct] = twoOptoGetData();
+dPrimes = [masterStruct.dPrimes];
+control = [dPrimes(:).noOpto];
+V1d = [dPrimes(:).V1];
+SCd = [dPrimes(:).SC];
+twoOpto = [dPrimes(:).twoOpto];
+%% Probability Summation Model
+
+% According to signal summation, the observed d' corresponds to:
+% d'= d' (V1) + d'(SC)
+
+% Range of d'
+d = 0:0.1:4; 
+% Grid it
+[X,Y] = meshgrid(d, d);
+% Signal Summation d'
+perfGrid = X + Y;
+
+% Make a plot
+figure; axis square; hold on;
+surf(perfGrid);
+xlabel('V1 d'''); ylabel('SC d''');
+set(gca, 'FontSize', 14); set(gca, 'TickDir', 'out');
+title('Control Performance Signal Summation');
+xlim([0 41]); ylim([0 41]); shading interp;
+set(gca, 'XTick', [0 10 20 30 40]); set(gca, 'YTick', [0 10 20 30 40]); 
+set(gca, 'XTickLabel', {'0', '1', '2', '3', '4'});
+set(gca, 'YTickLabel', {'0', '1', '2', '3', '4'});
+h = colorbar; h.Label.String = 'd'' Signal Summation';
+h.Label.Rotation = 270; h.Label.VerticalAlignment = "bottom";
+view(315,45); hold off;
+
+%%
+V1deltaD = nanmedian(control) - nanmedian(V1d);
+SCdeltaD = nanmedian(control) - nanmedian(SCd);
+[X2p, Y2p] = meshgrid(d-V1deltaD, d-SCdeltaD);
+predSS_grid = X2p + Y2p;
+
+figure; axis square; hold on;
+surf(predSS_grid);
+xlabel('V1 d'''); ylabel('SC d''');
+set(gca, 'FontSize', 14); set(gca, 'TickDir', 'out');
+title('TwoOpto Performance Signal Summation');
+xlim([0 41]); ylim([0 41]); shading interp;
+set(gca, 'XTick', [0 10 20 30 40]); set(gca, 'YTick', [0 10 20 30 40]); 
+set(gca, 'XTickLabel', {'0', '1', '2', '3', '4'});
+set(gca, 'YTickLabel', {'0', '1', '2', '3', '4'});
+h = colorbar; h.Label.String = 'd'' Signal Summation';
+h.Label.Rotation = 270; h.Label.VerticalAlignment = "bottom";
+view(315,45); hold off;
+
+%% Probability Summation
+
+% In probability summation model
+% d' = max(d'(V1), d'(SC))
+
+
+
+
+
 
 
 
